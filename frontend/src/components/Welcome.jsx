d835652333c22159a416658e92dc8465c8527c22
Ridlo21 <inurrie99@gmail.com>
2023-03-18 04:25:05 +0700

    Update Welcome.jsx

--- frontend/src/components/Welcome.jsx
+++ frontend/src/components/Welcome.jsx
@@ -11,13 +11,12 @@ const Welcome = () => {
             <h1 className='max-w-2xl mb-4 text-4xl font-extrabold leading-none tracking-tight md:text-5xl xl:text-6xl '>Selamat Datang Di<br /> SIAKAD</h1>
             <p className="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">
               Sekolah Tinggi Agama Islam Nurul Abror Al-Robbaniyin <br /> Jl. KH. Agus Salim 165 Alasbuluh Wongosrejo Banyuwangi
-
             </p>
             <div className="space-y-4 sm:flex sm:space-y-0 sm:space-x-4">
-              <Link to="/login" className="items-center justify-center w-full px-5 py-3 text-sm font-medium text-center text-gray-900 border rounded-lg sm:w-auto focus:ring-4 border border-[#2d8659] hover:bg-[#2d8659] hover:text-white">
+              <Link to="/login" className="items-center justify-center w-full px-5 py-3 text-sm font-medium text-center text-gray-900 rounded-lg sm:w-auto focus:ring-4 border border-[#2d8659] hover:bg-[#2d8659] hover:text-white">
                 Log in
               </Link>
-              <Link className="items-center justify-center w-full px-5 py-3 text-sm font-medium text-center text-gray-900 border rounded-lg sm:w-auto focus:ring-4 border border-[#2d8659] hover:bg-[#2d8659] hover:text-white">
+              <Link className="items-center justify-center w-full px-5 py-3 text-sm font-medium text-center text-gray-900 rounded-lg sm:w-auto focus:ring-4 border border-[#2d8659] hover:bg-[#2d8659] hover:text-white">
                 Register
               </Link>
             </div>
