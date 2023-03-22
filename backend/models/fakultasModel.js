aa309c5b9e03a9766bc01cea53384388f089b493
Arizal Iwan Mabruri <68585610+Iwanmabruri@users.noreply.github.com>
2023-03-23 00:53:54 +0700

    menambah field model fakultas dan prodi

--- backend/models/fakultasModel.js
+++ backend/models/fakultasModel.js
@@ -1,27 +1,30 @@
-const {Sequelize, DataTypes} = require('sequelize')
+const { Sequelize, DataTypes } = require('sequelize')
 const db = require('../config/database.js')
 const jenjangPendidikanModel = require('./jenjangPendidikanModel.js')
 
 const fakultasModel = db.define('fakultas', {
-    'id_fakultas' : {
+    'id_fakultas': {
         type: DataTypes.INTEGER,
         autoIncrement: true,
         primaryKey: true
     },
-    'code_jenjang_pendidikan' : {
-        type : DataTypes.TEXT,
+    'code_jenjang_pendidikan': {
+        type: DataTypes.TEXT,
     },
-    'code_fakultas' : {
-        type : DataTypes.TEXT
+    'code_fakultas': {
+        type: DataTypes.TEXT
     },
-    'nama_fakultas' : {
-        type : DataTypes.TEXT
+    'code_dikti_fakultas': {
+        type: DataTypes.TEXT
     },
-    'status' : {
-        type : DataTypes.ENUM,
-        values : ["aktif", "tidak aktif"]
+    'nama_fakultas': {
+        type: DataTypes.TEXT
+    },
+    'status': {
+        type: DataTypes.ENUM,
+        values: ["aktif", "tidak aktif"]
     }
-},{
+}, {
     tableName: 'tb_fakultas',
     freezeTableName: true,
     timestamps: false,
@@ -29,9 +32,9 @@ const fakultasModel = db.define('fakultas', {
     paranoid: true,
 })
 
-jenjangPendidikanModel.belongsTo(fakultasModel,{foreignKey: 'code_jenjang_pendidikan' })
-fakultasModel.hasMany(jenjangPendidikanModel,{ sourceKey: 'code_jenjang_pendidikan', foreignKey: 'code_jenjang_pendidikan' })
-    
+jenjangPendidikanModel.belongsTo(fakultasModel, { foreignKey: 'code_jenjang_pendidikan' })
+fakultasModel.hasMany(jenjangPendidikanModel, { sourceKey: 'code_jenjang_pendidikan', foreignKey: 'code_jenjang_pendidikan' })
+
 
 
 module.exports = fakultasModel
--- backend/models/prodiModel.js
+++ backend/models/prodiModel.js
@@ -1,31 +1,34 @@
-const {Sequelize, DataTypes} = require('sequelize')
+const { Sequelize, DataTypes } = require('sequelize')
 const db = require('../config/database.js')
 const jenjangPendidikanModel = require('./jenjangPendidikanModel.js')
 const fakultasModel = require('./fakultasModel.js')
 
 const prodiModel = db.define('prodi', {
-    'id_prodi' : {
+    'id_prodi': {
         type: DataTypes.INTEGER,
         autoIncrement: true,
         primaryKey: true
     },
-    'code_jenjang_pendidikan' : {
-        type : DataTypes.TEXT,
+    'code_jenjang_pendidikan': {
+        type: DataTypes.TEXT,
     },
-    'code_fakultas' : {
-        type : DataTypes.TEXT
+    'code_fakultas': {
+        type: DataTypes.TEXT
     },
-    'code_prodi' : {
-        type : DataTypes.TEXT
+    'code_prodi': {
+        type: DataTypes.TEXT
     },
-    'nama_prodi' : {
-        type : DataTypes.TEXT
+    'code_dikti_prodi': {
+        type: DataTypes.TEXT
     },
-    'status' : {
-        type : DataTypes.ENUM,
+    'nama_prodi': {
+        type: DataTypes.TEXT
+    },
+    'status': {
+        type: DataTypes.ENUM,
         values: ['aktif', 'tidak']
     }
-},{
+}, {
     tableName: 'tb_prodi',
     freezeTableName: true,
     timestamps: false,
@@ -33,12 +36,12 @@ const prodiModel = db.define('prodi', {
     paranoid: true,
 })
 
-jenjangPendidikanModel.belongsTo(prodiModel,{foreignKey: 'code_jenjang_pendidikan' })
-prodiModel.hasMany(jenjangPendidikanModel,{ sourceKey: 'code_jenjang_pendidikan', foreignKey: 'code_jenjang_pendidikan' })
+jenjangPendidikanModel.belongsTo(prodiModel, { foreignKey: 'code_jenjang_pendidikan' })
+prodiModel.hasMany(jenjangPendidikanModel, { sourceKey: 'code_jenjang_pendidikan', foreignKey: 'code_jenjang_pendidikan' })
 
-fakultasModel.belongsTo(prodiModel,{foreignKey: 'code_fakultas' })
-prodiModel.hasMany(fakultasModel,{ sourceKey: 'code_fakultas', foreignKey: 'code_fakultas' })
-    
+fakultasModel.belongsTo(prodiModel, { foreignKey: 'code_fakultas' })
+prodiModel.hasMany(fakultasModel, { sourceKey: 'code_fakultas', foreignKey: 'code_fakultas' })
+
 
 
 module.exports = prodiModel
