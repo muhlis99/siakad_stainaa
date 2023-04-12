ecf3566577b813ce2e443fa7879d4b588b6688a5
Ridlo21 <inurrie99@gmail.com>
2023-04-12 21:18:21 +0700

    crud jenjang

--- frontend/src/components/ListJenjang.jsx
+++ frontend/src/components/ListJenjang.jsx
@@ -1,17 +1,15 @@
 import React, { useState, useEffect } from 'react'
 import axios from "axios";
 import { BiEdit } from "react-icons/bi";
-import { FaSearch, FaTrash, FaArrowLeft, FaArrowRight, FaPlus } from "react-icons/fa";
+import { FaTimes, FaSearch, FaTrash, FaArrowLeft, FaArrowRight, FaPlus } from "react-icons/fa";
 import { SlOptions } from "react-icons/sl";
 import ReactPaginate from "react-paginate";
-import { Link, useNavigate } from 'react-router-dom';
 import Swal from "sweetalert2";
 
 const ListJenjang = () => {
     const [Jenjang, setJenjang] = useState([]);
     const [page, setPage] = useState(0);
     const [perPage, setperPage] = useState(0);
-    // const [limit, setLimit] = useState(10);
     const [pages, setPages] = useState(0);
     const [rows, setrows] = useState(0);
     const [keyword, setKeyword] = useState("");
@@ -19,7 +17,7 @@ const ListJenjang = () => {
     const [msg, setMsg] = useState("");
     const [nama, setNama] = useState("");
     const [errors, setError] = useState("");
-    // const navigate = useNavigate();
+    const [id, setId] = useState("");
 
     useEffect(() => {
         getJenjang()
@@ -60,7 +58,7 @@ const ListJenjang = () => {
             if (nama.length == 0) {
                 setError(true)
             } else {
-                document.getElementById('my-modal').checked = false;
+                document.getElementById('my-modal-add').checked = false;
                 await axios.post(
                     'v1/jenjangPendidikan/create', {
                     nama_jenjang_pendidikan: nama
@@ -71,44 +69,173 @@ const ListJenjang = () => {
                         icon: "success"
                     }).then(() => {
                         getJenjang()
-                        document.getElementById('nama')
+                        setNama("")
                     });
                 })
 
             }
-
         } catch (error) {
-
+            if (error.response) {
+                setError(true)
+            }
         }
+    }
+
+    const modalAddClose = (e) => {
+        e.preventDefault()
+        document.getElementById('my-modal-add').checked = false;
+        setNama("")
+        setError(false)
+    }
+
+    const modalEditOpen = async (e) => {
+        try {
+            const response = await axios.get(`v1/jenjangPendidikan/getById/${e}`);
+            setNama(response.data.data.nama_jenjang_pendidikan)
+            document.getElementById('my-modal-edit').checked = true;
+        } catch (error) {
+        }
+    }
+
+    const modalEditClose = (e) => {
+        e.preventDefault()
+        document.getElementById('my-modal-edit').checked = false;
+        setNama("")
+        setId("")
+        setError(false)
+    }
+
+    const updateJenjang = async (e) => {
+        e.preventDefault()
+        try {
+            if (nama.length == 0) {
+                setError(true)
+            } else {
+                document.getElementById('my-modal-edit').checked = false;
+                await axios.put(
+                    `v1/jenjangPendidikan/update/${id}`, {
+                    nama_jenjang_pendidikan: nama
+                }).then(function (response) {
+                    Swal.fire({
+                        title: "Berhasil",
+                        text: response.data.message,
+                        icon: "success"
+                    }).then(() => {
+                        getJenjang()
+                        setNama("")
+                        setId("")
+                    });
+                })
+            }
+        } catch (error) {
+            if (error.response) {
+                setError(true)
+            }
+        }
+
+    }
+
+    const nonaktifkan = (jenjangId) => {
+        Swal.fire({
+            title: "Hapus data ini?",
+            text: "Anda tidak dapat mengembalikan ini",
+            icon: "question",
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Ya, hapus!',
+            cancelButtonText: 'Batal'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                try {
+                    axios.put(
+                        `v1/jenjangPendidikan/deleteStatus/${jenjangId}`
+                    ).then((response) => {
+                        console.log(response.data)
+                        // Swal.fire({
+                        //     title: "",
+                        //     text: "File Anda telah dihapus.",
+                        //     icon: "success"
+                        // }).then(() => {
+                        //     getMasuk()
+                        // });
+                    })
+
+                } catch (error) {
+
+                }
+            }
+        })
     }
 
     return (
         <div className="container mt-2">
-            <input type="checkbox" id="my-modal" className="modal-toggle" />
+            {/* Modal untuk tambah data */}
+            <input type="checkbox" id="my-modal-add" className="modal-toggle" />
             <div className="modal">
-                <div className="modal-box">
+                <div className="modal-box relative">
+                    <button className="btn btn-sm btn-circle btn-danger absolute right-2 top-2" onClick={modalAddClose}><FaTimes /></button>
                     <form onSubmit={simpanJenjang}>
                         <h3 className="font-bold text-xl">Tambah Jenjang Pendidikan</h3>
-                        <p className="py-4">
+                        <div className="py-4">
                             <label className=' uppercase font-bold'>Nama Jenjang Pendidikan</label>
-                            <input
-                                type="text"
-                                id='nama'
-                                value={nama}
-                                onChange={(e) => setNama(e.target.value)}
-                                placeholder="Nama Jenjang Pendidikan"
-                                className="mt-2 input input-sm input-bordered w-full"
-                            />
+                            <div className="form-control w-full ">
+                                <select
+                                    className="select select-bordered select-sm"
+                                    value={nama}
+                                    onChange={(e) => setNama(e.target.value)}
+                                >
+                                    <option disabled value={""}>-Pilih Jenjang Pendidikan-</option>
+                                    <option>DIPLOMA</option>
+                                    <option>SARJANA</option>
+                                    <option>MAGISTER</option>
+                                    <option>DOKTOR</option>
+                                </select>
+                            </div>
                             {errors && nama.length <= 0 ?
                                 <span className='mt-2 text-red-700 text-xs'>Tidak Boleh Kosong</span> : ""
                             }
-                        </p>
+                        </div>
                         <div className="modal-action">
                             <button type='submit' className="btn btn-sm btn-default">simpan</button>
                         </div>
                     </form>
                 </div>
             </div>
+
+            {/* Modal untuk edit data */}
+            <input type="checkbox" id="my-modal-edit" className="modal-toggle" />
+            <div className="modal">
+                <div className="modal-box relative">
+                    <button className="btn btn-sm btn-circle btn-danger absolute right-2 top-2" onClick={modalEditClose}><FaTimes /></button>
+                    <form onSubmit={updateJenjang}>
+                        <h3 className="font-bold text-xl">Edit Jenjang Pendidikan</h3>
+                        <div className="py-4">
+                            <label className=' uppercase font-bold'>Nama Jenjang Pendidikan</label>
+                            <div className="form-control w-full ">
+                                <select
+                                    className="select select-bordered select-sm"
+                                    value={nama}
+                                    onChange={(e) => setNama(e.target.value)}
+                                >
+                                    <option disabled value={""}>-Pilih Jenjang Pendidikan-</option>
+                                    <option>DIPLOMA</option>
+                                    <option>SARJANA</option>
+                                    <option>MAGISTER</option>
+                                    <option>DOKTOR</option>
+                                </select>
+                            </div>
+                            {errors && nama.length <= 0 ?
+                                <span className='mt-2 text-red-700 text-xs'>Tidak Boleh Kosong</span> : ""
+                            }
+                        </div>
+                        <div className="modal-action">
+                            <button type='submit' className="btn btn-sm btn-default">simpan</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+
             <section className='mb-7'>
                 <h1 className='text-2xl font-bold'>Jenjang Pendidikan</h1>
             </section>
@@ -117,8 +244,7 @@ const ListJenjang = () => {
                     <div className="card-body">
                         <div className="flex">
                             <div className="flex-1">
-                                <label htmlFor="my-modal" className="btn btn-default btn-sm"><FaPlus /> open modal</label>
-                                {/* <Link to="/jenjang/add" className='btn btn-default btn-sm'><FaPlus /> tambah data</Link> */}
+                                <label htmlFor="my-modal-add" className="btn btn-default btn-sm"><FaPlus /> tambah data</label>
                             </div>
                         </div>
                         <form onSubmit={cariData} className='mb-1'>
@@ -157,8 +283,8 @@ const ListJenjang = () => {
                                             <td className='px-6 py-4'>{jenj.nama_jenjang_pendidikan}</td>
                                             <td className='px-6 py-4' align='center'>
                                                 <div className="btn-group">
-                                                    <Link to={`/jenjang/edit/${jenj.id_jenjang_pendidikan}`} className="btn btn-sm text-white btn-warning" title='Edit'><FaTrash /></Link>
-                                                    <button className="btn btn-sm text-white btn-danger" title='Hapus'><BiEdit /></button>
+                                                    <button className="btn btn-sm text-white btn-warning" onClick={() => modalEditOpen(jenj.id_jenjang_pendidikan)} title='Edit'><BiEdit /></button>
+                                                    <button className="btn btn-sm text-white btn-danger" onClick={() => nonaktifkan(jenj.id_jenjang_pendidikan)} title='Hapus'><FaTrash /></button>
                                                 </div>
                                             </td>
                                         </tr>
@@ -167,8 +293,8 @@ const ListJenjang = () => {
                             </table>
                         </div>
                         <div>
-                            <p>Total Data : {rows} page: {rows ? page : 0} of {pages}</p>
-                            <p className='text-sm text-red-700'>{msg}</p>
+                            <span>Total Data : {rows} page: {rows ? page : 0} of {pages}</span>
+                            <span className='text-sm text-red-700'>{msg}</span>
                         </div>
                         <div className="mt-3 justify-center btn-group" key={rows} aria-label='pagination'>
                             <ReactPaginate
@@ -189,7 +315,7 @@ const ListJenjang = () => {
                     </div>
                 </div>
             </section>
-        </div>
+        </div >
     )
 }
 
