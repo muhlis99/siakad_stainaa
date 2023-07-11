a9435f490b20b22dc44e580914a6f0349bb4a0c4
muhlis99 <72523345+muhlis99@users.noreply.github.com>
2023-07-12 01:43:03 +0700

    ok

--- backend/router/krsRoute.js
+++ backend/router/krsRoute.js
@@ -4,7 +4,7 @@ const krsController = require('../controllers/krsController.js')
 // const { validationkrs } = require('../validation/validationkrs.js')
 // const { validationRequest } = require('../validation/validationRequest.js')
 
-router.get('/all/:thnAjr/:prd/:smt', krsController.getAll)
+router.get('/all', krsController.getAll)
 router.get('/viewAll/:thnAjr/:prd/:smt', krsController.viewAll)
 router.post('/create/:thnAjr/:prd/:smt', krsController.post)
 
