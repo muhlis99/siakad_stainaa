592707d40679ea1174c013f25eb743ec89a8b826
muhlis99 <72523345+muhlis99@users.noreply.github.com>
2023-07-23 05:59:45 +0700

    home

---
+++ backend/router/homeRoute.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const route = express.Router()
+const homeController = require('../controllers/homeController.js')
+
+
+route.get('/totalMahasiswaPutera', homeController.totalMahasiswaPutera)
+route.get('/totalMahasiswaPuteri', homeController.totalMahasiswaPuteri)
+route.get('/totalDosen', homeController.totalDosen)
+route.get('/totalProdi', homeController.totalProdi)
+route.get('/diagramMahasiswa', homeController.diagramMahasiswa)
+
+module.exports = route
