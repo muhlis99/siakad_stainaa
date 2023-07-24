87505eea8b3bff561536b157a6b4189b7114dc45
Ridlo21 <inurrie99@gmail.com>
2023-07-25 00:53:07 +0700

    dashboard

--- frontend/src/components/Beranda.jsx
+++ frontend/src/components/Beranda.jsx
@@ -1,19 +1,83 @@
-import React from 'react'
-import { FaBookmark, FaChalkboardTeacher, FaUniversity, FaUserGraduate } from 'react-icons/fa'
+import React, { useState, useEffect } from 'react'
+import { FaBookmark, FaChalkboardTeacher, FaUserGraduate } from 'react-icons/fa'
+import axios from "axios"
+import Chart from "chart.js/auto"
+import { Bar } from "react-chartjs-2"
 
 const Beranda = () => {
+    const [putera, setPutera] = useState("")
+    const [puteri, setPuteri] = useState("")
+    const [dosen, setDosen] = useState("")
+    const [prodi, setProdi] = useState("")
+    const [Diagram, setDiagram] = useState([])
+
+    useEffect(() => {
+        getMhsPutera()
+        getMhsPuteri()
+        getDosen()
+        getProdi()
+        getDiagramMhs()
+    }, [])
+
+    const getMhsPutera = async () => {
+        const response = await axios.get('v1/home/totalMahasiswaPutera')
+        setPutera(response.data.data)
+    }
+
+    const getMhsPuteri = async () => {
+        const response = await axios.get('v1/home/totalMahasiswaPuteri')
+        setPuteri(response.data.data)
+    }
+
+    const getDosen = async () => {
+        const response = await axios.get('v1/home/totalDosen')
+        setDosen(response.data.data)
+    }
+
+    const getProdi = async () => {
+        const response = await axios.get('v1/home/totalProdi')
+        setProdi(response.data.data)
+    }
+
+    const getDiagramMhs = async () => {
+        const response = await axios.get('v1/home/diagramMahasiswa')
+        setDiagram(response.data.data);
+    }
+
+    const labels = Diagram.map(item => (
+        item.tahun
+    ))
+
+    const Jumlah = Diagram.map(item => (
+        item.jumlahMahasiswa
+    ))
+
+    const data = {
+        labels: labels,
+        datasets: [
+            {
+                label: "Mahasiswa pertahun",
+                backgroundColor: "rgb(28, 118, 253, 0.7)",
+                borderColor: "rgb(28, 118, 253)",
+                borderWidth: 2,
+                borderRadius: 5,
+                data: Jumlah,
+            },
+        ],
+    }
+
     return (
         <div className="container mt-2">
             <section className='mb-5'>
                 <h1 className='text-2xl font-bold'>Dashboard</h1>
             </section>
             <section>
-                <div className="grid grid-cols-4 gap-4">
+                <div className="grid grid-cols-4 gap-4 mb-4">
                     <div>
-                        <div className='w-full h-36 bg-[#60B033] hover:bg-[#4c8a28] hover:shadow-lg rounded-md px-3 py-3 grid grid-cols-2'>
-                            <div>
+                        <div className='w-full h-36 bg-[#60B033] hover:bg-[#4c8a28] hover:shadow-lg rounded-md px-3 py-3 grid grid-cols-3'>
+                            <div className='col-span-2'>
                                 <h1 className='text-md text-white'>Mahasiswa Putera</h1>
-                                <h1 className='text-3xl text-white font-bold mt-2'>1</h1>
+                                <h1 className='text-3xl text-white font-bold mt-2'>{putera}</h1>
                             </div>
                             <div>
                                 <h1 className='text-white text-4xl float-right'><FaUserGraduate /></h1>
@@ -21,10 +85,10 @@ const Beranda = () => {
                         </div>
                     </div>
                     <div>
-                        <div className='w-full h-36 bg-[#725648] rounded-md px-3 py-3 grid grid-cols-2'>
-                            <div>
+                        <div className='w-full h-36 bg-[#725648] rounded-md px-3 py-3 grid grid-cols-3'>
+                            <div className='col-span-2'>
                                 <h1 className='text-md text-white'>Mahasiswa Puteri</h1>
-                                <h1 className='text-3xl text-white font-bold mt-2'>1</h1>
+                                <h1 className='text-3xl text-white font-bold mt-2'>{puteri}</h1>
                             </div>
                             <div>
                                 <h1 className='text-white text-4xl float-right'><FaUserGraduate /></h1>
@@ -35,7 +99,7 @@ const Beranda = () => {
                         <div className='w-full h-36 bg-[#D4C403] rounded-md px-3 py-3 grid grid-cols-2'>
                             <div>
                                 <h1 className='text-md text-white'>Dosen</h1>
-                                <h1 className='text-3xl text-white font-bold mt-2'>1</h1>
+                                <h1 className='text-3xl text-white font-bold mt-2'>{dosen}</h1>
                             </div>
                             <div>
                                 <h1 className='text-white text-4xl float-right'><FaChalkboardTeacher /></h1>
@@ -45,8 +109,8 @@ const Beranda = () => {
                     <div>
                         <div className='w-full h-36 bg-[#2D677F] rounded-md px-3 py-3 grid grid-cols-2'>
                             <div>
-                                <h1 className='text-md text-white'>Total Prodi</h1>
-                                <h1 className='text-3xl text-white font-bold mt-2'>1</h1>
+                                <h1 className='text-md text-white'>Program Studi</h1>
+                                <h1 className='text-3xl text-white font-bold mt-2'>{prodi}</h1>
                             </div>
                             <div>
                                 <h1 className='text-white text-4xl float-right'><FaBookmark /></h1>
@@ -54,6 +118,11 @@ const Beranda = () => {
                         </div>
                     </div>
 
+                </div>
+                <div className="card bg-base-100 card-bordered shadow-md mb-2 rounded-md">
+                    <div className="card-body p-4">
+                        <Bar data={data} />
+                    </div>
                 </div>
             </section>
         </div>
