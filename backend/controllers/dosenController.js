b8ed99d9af45f4fe9b00cbb484648305d02e9fbf
muhlis99 <72523345+muhlis99@users.noreply.github.com>
2023-08-06 02:05:40 +0700

    update mahasiswa dan dosen

--- backend/controllers/dosenController.js
+++ backend/controllers/dosenController.js
@@ -254,7 +254,7 @@ module.exports = {
             const ctx = canvas.getContext("2d")
             const img = await loadImage(center_image)
             const center = (width - cwidth) / 1
-            ctx.drawImage(img, 170, 180, cwidth, cwidth)
+            ctx.drawImage(img, 200, 190, cwidth, cwidth)
             return canvas.toDataURL("image/png")
         }
 
@@ -268,8 +268,8 @@ module.exports = {
                 const qrCode = await createQrCode(
                     data,
                     `data:image/png;base64,${dataQrWithLogo}`,
-                    250,
-                    150
+                    150,
+                    100
                 )
                 const base64Data = qrCode.replace(/^data:image\/png;base64,/, "");
                 fs.unlinkSync(`tmp/dosen/qrcode/${qrCodeOld}`)
@@ -286,8 +286,8 @@ module.exports = {
                 const qrCode = await createQrCode(
                     data,
                     `data:image/png;base64,${dataQrWithLogo}`,
-                    250,
-                    150
+                    150,
+                    100
                 )
                 const base64Data = qrCode.replace(/^data:image\/png;base64,/, "");
                 let filename = `tmp/dosen/qrcode/${qrCodeNew}.png`;
