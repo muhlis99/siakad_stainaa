644f29b42b7abaf70670df8ee644d4f11222e799
muhlis99 <72523345+muhlis99@users.noreply.github.com>
2023-08-20 01:37:37 +0700

    coba socket io

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,11 @@ const app = express()
 const http = require('http')
 const server = http.createServer(app)
 const { Server } = require('socket.io')
-const io = new Server(server)
+const io = new Server(server, {
+    cors: {
+        origin: "*",
+    }
+})
 const sessionStore = sequelizeStore(session.Store)
 const store = new sessionStore({
     db: db
