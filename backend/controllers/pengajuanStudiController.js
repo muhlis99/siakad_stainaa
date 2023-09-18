a4dd60993e1d5008e4ab84feb1296d233e6925d1
muhlis99 <72523345+muhlis99@users.noreply.github.com>
2023-09-19 01:35:09 +0700

    pengajuan studi mahasiswa done

--- backend/controllers/pengajuanStudiController.js
+++ backend/controllers/pengajuanStudiController.js
@@ -456,66 +456,6 @@ module.exports = {
     },
 
 
-    postMahasiswa: async (req, res, next) => {
-        const { code_tahun_ajaran, code_semester, code_jenjang_pendidikan, code_fakultas, code_prodi,
-            nim, tanggal_pengajuan, pengajuan, alasan } = req.body
-
-        const dataMahasiswaUse = await historyMahasiswa.findOne({
-            include: [{
-                model: tahunAjaranModel,
-                where: { status: "aktif" }
-            }, {
-                model: semesterModel,
-                where: { status: "aktif" }
-            }, {
-                model: jenjangPendidikanModel,
-                where: { status: "aktif" }
-            }, {
-                model: fakultasModel,
-                where: { status: "aktif" }
-            }, {
-                model: prodiModel,
-                where: { status: "aktif" }
-            }],
-            where: {
-                code_tahun_ajaran: code_tahun_ajaran,
-                code_semester: code_semester,
-                code_jenjang_pendidikan: code_jenjang_pendidikan,
-                code_fakultas: code_fakultas,
-                code_prodi: code_prodi,
-                nim: nim,
-                status: {
-                    [Op.notIn]: ['tidak', 'berhenti']
-                }
-            }
-        })
-
-        if (!dataMahasiswaUse) return res.status(404).json({ message: "Mahasiswa sudah berhenti" })
-
-        let randomNumber = Math.floor(100000 + Math.random() * 900000)
-        await pengajuanStudi.create({
-            code_pengajuan_studi: randomNumber,
-            code_tahun_ajaran: code_tahun_ajaran,
-            code_semester: code_semester,
-            code_jenjang_pendidikan: code_jenjang_pendidikan,
-            code_fakultas: code_fakultas,
-            code_prodi: code_prodi,
-            nim: nim,
-            tanggal_pengajuan: tanggal_pengajuan,
-            pengajuan: pengajuan,
-            alasan: alasan,
-            status: "proses"
-        }).
-            then(result => {
-                res.status(201).json({
-                    message: "Data pengajuan succes Ditambahkan",
-                })
-            }).
-            catch(err => {
-                next(err)
-            })
-    },
-
     createAdmin: async (req, res, next) => {
         const { code_tahun_ajaran, code_semester, code_jenjang_pendidikan, code_fakultas, code_prodi,
             nim, tanggal_pengajuan, pengajuan, alasan } = req.body
@@ -853,5 +793,387 @@ module.exports = {
             catch(err => {
                 next(err)
             })
-    }
+    },
+
+    // user mahasiswa
+    allMahasiswa: async (req, res, next) => {
+        const nim = req.params.nim
+        const currentPage = parseInt(req.query.page) || 1
+        const perPage = parseInt(req.query.perPage) || 10
+        const search = req.query.search || ""
+        const offset = (currentPage - 1) * perPage
+        const totalPage = await pengajuanStudi.count({
+            include: [{
+                attributes: ['nim', 'nama'],
+                model: mahasiswaModel,
+                where: { status: "aktif" }
+            }, {
+                model: tahunAjaranModel,
+                attributes: ['tahun_ajaran'],
+                where: { status: "aktif" }
+            }, {
+                attributes: ['semester'],
+                model: semesterModel,
+                where: { status: "aktif" }
+            }, {
+                model: jenjangPendidikanModel,
+                attributes: ['nama_jenjang_pendidikan'],
+                where: { status: "aktif" }
+            }, {
+                attributes: ['nama_fakultas'],
+                model: fakultasModel,
+                where: { status: "aktif" }
+            }, {
+                attributes: ['nama_prodi'],
+                model: prodiModel,
+                where: { status: "aktif" }
+            }],
+            where: {
+                nim: nim,
+                [Op.or]: [
+                    {
+                        id_pengajuan_studi: {
+                            [Op.like]: `%${search}%`
+                        }
+                    },
+                    {
+                        nim: {
+                            [Op.like]: `%${search}%`
+                        }
+                    },
+                    {
+                        tanggal_pengajuan: {
+                            [Op.like]: `%${search}%`
+                        }
+                    },
+                    {
+                        pengajuan: {
+                            [Op.like]: `%${search}%`
+                        }
+                    },
+                    {
+                        alasan: {
+                            [Op.like]: `%${search}%`
+                        }
+                    }
+                ]
+            }
+        })
+        const totalItems = Math.ceil(totalPage / perPage)
+        await pengajuanStudi.findAll({
+            include: [{
+                attributes: ['nim', 'nama'],
+                model: mahasiswaModel,
+                where: { status: "aktif" }
+            }, {
+                model: tahunAjaranModel,
+                attributes: ['tahun_ajaran'],
+                where: { status: "aktif" }
+            }, {
+                attributes: ['semester'],
+                model: semesterModel,
+                where: { status: "aktif" }
+            }, {
+                model: jenjangPendidikanModel,
+                attributes: ['nama_jenjang_pendidikan'],
+                where: { status: "aktif" }
+            }, {
+                attributes: ['nama_fakultas'],
+                model: fakultasModel,
+                where: { status: "aktif" }
+            }, {
+                attributes: ['nama_prodi'],
+                model: prodiModel,
+                where: { status: "aktif" }
+            }],
+            where: {
+                nim: nim,
+                [Op.or]: [
+                    {
+                        id_pengajuan_studi: {
+                            [Op.like]: `%${search}%`
+                        }
+                    },
+                    {
+                        nim: {
+                            [Op.like]: `%${search}%`
+                        }
+                    },
+                    {
+                        tanggal_pengajuan: {
+                            [Op.like]: `%${search}%`
+                        }
+                    },
+                    {
+                        pengajuan: {
+                            [Op.like]: `%${search}%`
+                        }
+                    },
+                    {
+                        alasan: {
+                            [Op.like]: `%${search}%`
+                        }
+                    }
+                ]
+            },
+            offset: offset,
+            limit: perPage,
+            order: [
+                ["id_pengajuan_studi", "DESC"]
+            ]
+        }).
+            then(result => {
+                res.status(200).json({
+                    message: "Get All pengajun studi Success",
+                    data: result,
+                    total_data: totalPage,
+                    per_page: perPage,
+                    current_page: currentPage,
+                    total_page: totalItems
+                })
+            }).
+            catch(err => {
+                next(err)
+            })
+    },
+
+    getMahasiswaById: async (req, res, next) => {
+        const id = req.params.id
+        await pengajuanStudi.findOne({
+            include: [{
+                attributes: ['nim', 'nama'],
+                model: mahasiswaModel,
+                where: { status: "aktif" }
+            }, {
+                model: tahunAjaranModel,
+                attributes: ['tahun_ajaran'],
+                where: { status: "aktif" }
+            }, {
+                attributes: ['semester'],
+                model: semesterModel,
+                where: { status: "aktif" }
+            }, {
+                model: jenjangPendidikanModel,
+                attributes: ['nama_jenjang_pendidikan'],
+                where: { status: "aktif" }
+            }, {
+                attributes: ['nama_fakultas'],
+                model: fakultasModel,
+                where: { status: "aktif" }
+            }, {
+                attributes: ['nama_prodi'],
+                model: prodiModel,
+                where: { status: "aktif" }
+            }],
+            where: {
+                id_pengajuan_studi: id,
+            }
+        }).
+            then(result => {
+                if (!result) {
+                    return res.status(404).json({
+                        message: "Data pengajuan studi Tidak Ditemukan",
+                        data: []
+                    })
+                }
+                res.status(201).json({
+                    message: "Data pengajuan studi Ditemukan",
+                    data: result
+                })
+            }).
+            catch(err => {
+                next(err)
+            })
+    },
+
+    postMahasiswa: async (req, res, next) => {
+        const { code_tahun_ajaran, code_semester, code_jenjang_pendidikan, code_fakultas, code_prodi,
+            nim, tanggal_pengajuan, pengajuan, alasan } = req.body
+
+        const dataMahasiswaUse = await historyMahasiswa.findOne({
+            include: [{
+                model: tahunAjaranModel,
+                where: { status: "aktif" }
+            }, {
+                model: semesterModel,
+                where: { status: "aktif" }
+            }, {
+                model: jenjangPendidikanModel,
+                where: { status: "aktif" }
+            }, {
+                model: fakultasModel,
+                where: { status: "aktif" }
+            }, {
+                model: prodiModel,
+                where: { status: "aktif" }
+            }],
+            where: {
+                code_tahun_ajaran: code_tahun_ajaran,
+                code_semester: code_semester,
+                code_jenjang_pendidikan: code_jenjang_pendidikan,
+                code_fakultas: code_fakultas,
+                code_prodi: code_prodi,
+                nim: nim,
+                status: {
+                    [Op.notIn]: ['tidak', 'berhenti']
+                }
+            }
+        })
+        if (!dataMahasiswaUse) return res.status(404).json({ message: "Mahasiswa sudah berhenti / tidak ditemukan" })
+
+        let randomNumber = Math.floor(100000 + Math.random() * 900000)
+        await pengajuanStudi.create({
+            code_pengajuan_studi: randomNumber,
+            code_tahun_ajaran: code_tahun_ajaran,
+            code_semester: code_semester,
+            code_jenjang_pendidikan: code_jenjang_pendidikan,
+            code_fakultas: code_fakultas,
+            code_prodi: code_prodi,
+            nim: nim,
+            tanggal_pengajuan: tanggal_pengajuan,
+            pengajuan: pengajuan,
+            alasan: alasan,
+            status: "proses"
+        }).
+            then(result => {
+                res.status(201).json({
+                    message: "Data pengajuan succes Ditambahkan",
+                })
+            }).
+            catch(err => {
+                next(err)
+            })
+    },
+
+    putMahasiswa: async (req, res, next) => {
+        const id = req.params.id
+        const pengajuanUse = await pengajuanStudi.findOne({
+            include: [{
+                attributes: ['nim', 'nama'],
+                model: mahasiswaModel,
+                where: { status: "aktif" }
+            }, {
+                model: tahunAjaranModel,
+                attributes: ['tahun_ajaran'],
+                where: { status: "aktif" }
+            }, {
+                attributes: ['semester'],
+                model: semesterModel,
+                where: { status: "aktif" }
+            }, {
+                model: jenjangPendidikanModel,
+                attributes: ['nama_jenjang_pendidikan'],
+                where: { status: "aktif" }
+            }, {
+                attributes: ['nama_fakultas'],
+                model: fakultasModel,
+                where: { status: "aktif" }
+            }, {
+                attributes: ['nama_prodi'],
+                model: prodiModel,
+                where: { status: "aktif" }
+            }],
+            where: {
+                id_pengajuan_studi: id,
+            }
+        })
+        if (!pengajuanUse) return res.status(404).json({ message: "data tidak ditemukan" })
+        const { code_tahun_ajaran, code_semester, code_jenjang_pendidikan, code_fakultas, code_prodi,
+            nim, pengajuan, alasan } = req.body
+
+        const dataMahasiswaUse = await historyMahasiswa.findOne({
+            include: [{
+                model: tahunAjaranModel,
+                where: { status: "aktif" }
+            }, {
+                model: semesterModel,
+                where: { status: "aktif" }
+            }, {
+                model: jenjangPendidikanModel,
+                where: { status: "aktif" }
+            }, {
+                model: fakultasModel,
+                where: { status: "aktif" }
+            }, {
+                model: prodiModel,
+                where: { status: "aktif" }
+            }],
+            where: {
+                code_tahun_ajaran: code_tahun_ajaran,
+                code_semester: code_semester,
+                code_jenjang_pendidikan: code_jenjang_pendidikan,
+                code_fakultas: code_fakultas,
+                code_prodi: code_prodi,
+                nim: nim,
+                status: {
+                    [Op.notIn]: ['tidak', 'berhenti']
+                }
+            }
+        })
+        if (!dataMahasiswaUse) return res.status(404).json({ message: "Mahasiswa sudah berhenti / tidak ditemukan" })
+        await pengajuanStudi.update({
+            pengajuan: pengajuan,
+            alasan: alasan,
+        }, {
+            where: {
+                id_pengajuan_studi: id
+            }
+        }).
+            then(result => {
+                res.status(201).json({
+                    message: "Data pengajuan succes Dirubah",
+                })
+            }).
+            catch(err => {
+                next(err)
+            })
+    },
+
+    deleteMahasiswa: async (req, res, next) => {
+        const id = req.params.id
+        const pengajuanStudiUse = await pengajuanStudi.findOne({
+            include: [{
+                attributes: ['nim', 'nama'],
+                model: mahasiswaModel,
+                where: { status: "aktif" }
+            }, {
+                model: tahunAjaranModel,
+                where: { status: "aktif" }
+            }, {
+                model: semesterModel,
+                where: { status: "aktif" }
+            }, {
+                model: jenjangPendidikanModel,
+                where: { status: "aktif" }
+            }, {
+                model: fakultasModel,
+                where: { status: "aktif" }
+            }, {
+                model: prodiModel,
+                where: { status: "aktif" }
+            }],
+            where: {
+                id_pengajuan_studi: id,
+                status: {
+                    [Op.ne]: 'tidak'
+                }
+            }
+        })
+        if (!pengajuanStudiUse) return res.status(401).json({ message: "Data Pengajuan Studi tidak ditemukan" })
+        await pengajuanStudiUse.update({
+            status: "tidak"
+        }, {
+            where: {
+                id_pengajuan_studi: id
+            }
+        }).
+            then(result => {
+                res.status(201).json({
+                    message: "data Pengajuan Studi succes dihapus"
+                })
+            }).
+            catch(err => {
+                next(err)
+            })
+    },
 }
--- backend/router/pengajuanStudiRoute.js
+++ backend/router/pengajuanStudiRoute.js
@@ -7,12 +7,20 @@ const { validationRequest } = require('../validation/validationRequest.js')
 router.get('/all', pengajuanStudiController.get)
 router.get('/allAdmin', pengajuanStudiController.getAdmin)
 router.get('/getById/:id', pengajuanStudiController.getById)
-router.post('/createMahasiswa', validationPengajuanStudi, validationRequest, pengajuanStudiController.postMahasiswa)
+// router.post('/createMahasiswa', validationPengajuanStudi, validationRequest, pengajuanStudiController.postMahasiswa)
 router.post('/createAdmin', validationPengajuanStudi, validationRequest, pengajuanStudiController.createAdmin)
 router.put('/approveDosen/:id', pengajuanStudiController.approveDosen)
 router.put('/approveBuak/:id', pengajuanStudiController.approveBuak)
 router.put('/deleteStatus/:id', pengajuanStudiController.deleteStatus)
 router.get('/autocomplete/:codeThnAjr/:codeSmt/:codeJnjPen/:codeFks/:codePrd/:status', pengajuanStudiController.autocomplete)
+
+//  user mahasiswa
+router.get('/allMahasiswa/:nim', pengajuanStudiController.allMahasiswa)
+router.get('/getMahasiswaById/:id', pengajuanStudiController.getMahasiswaById)
+router.post('/createMahasiswa', validationPengajuanStudi, validationRequest, pengajuanStudiController.postMahasiswa)
+router.put('/updateMahasiswa/:id', validationPengajuanStudi, validationRequest, pengajuanStudiController.putMahasiswa)
+router.put('/deleteMahasiswa/:id', pengajuanStudiController.deleteMahasiswa)
+
 
 
 module.exports = router
