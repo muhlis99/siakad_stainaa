c7ed86dc38017657deeb3c6e050f747374261373
Ridlo21 <inurrie99@gmail.com>
2023-12-02 04:42:32 +0700

    jadwal kuliah dan khs

--- frontend-dosen/src/pages/Login.jsx
+++ frontend-dosen/src/pages/Login.jsx
@@ -129,57 +129,6 @@ const Login = () => {
                             </form>
                         </div>
                     </div>
-                    {/* <div className='flex flex-col lg:flex-row w-full overflow-hidden py-7'>
-                    <div className="w-full lg:w-1/2 py-16 px-12 bg-no-repeat bg-auto bg-right bg-50%" style={{ backgroundImage: `url(${stainaa})`, backgroundSize: '180px' }}>
-
-                    </div>
-                    <div className="w-full lg:w-2/3 py-16 pr-52 pl-32">
-                        <h2 className='text-3xl mb-3 text-[#2D7F5F] font-bold'>SELAMAT DATANG</h2>
-                        <p className='text-gray-500'>Sistem Akademik Sekolah Tinggi <br />Agama Islam Nurul Abror Al-Robbaniyin</p>
-                        <form onSubmit={Auth} className='w-full'>
-
-                            <div className='mb-4'>
-                                <div className="relative mt-2 rounded-md shadow-sm">
-                                    <input
-                                        type="text"
-                                        id='username'
-                                        value={name}
-                                        onChange={(e) => setUsername(e.target.value)}
-                                        className='block w-full rounded-lg border py-1.5 pl-2 pr-20 text-gray-900 ring-1 ring-inset ring-[#2d8659] placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
-                                        placeholder='Username Anda'
-                                    />
-                                    <div className="absolute inset-y-0 right-0 flex items-center bg-[#2d8659] rounded-r-lg text-white px-3">
-                                        {<FaUserCircle />}
-                                    </div>
-                                </div>
-                            </div>
-                            <div className='mb-4'>
-                                <div className="relative mt-2 rounded-md shadow-sm">
-                                    <input
-                                        type={!isVisible ? "password" : "text"}
-                                        id='password'
-                                        value={password}
-                                        onChange={(e) => setPassword(e.target.value)}
-                                        className='block w-full rounded-lg border py-1.5 pl-2 pr-20 text-gray-900 ring-1 ring-inset ring-[#2d8659] placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
-                                        placeholder='Password Anda'
-                                    />
-                                    <div className="absolute inset-y-0 right-0 flex items-center bg-[#2d8659] rounded-r-lg text-white px-3 cursor-pointer" onClick={toggle}>
-                                        {isVisible ? <FaEyeSlash /> : <FaEye />}
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="mt-2 mb-4 float-right">
-                                <span className='text-xs'>
-                                    <Link to="/forgot" className='text-gray-500'>Forgot Password ?</Link>
-                                </span>
-                            </div>
-                            <div>
-                                <button type='submit' className='w-full bg-[#2D8659] p-2 rounded-md text-white'>Login</button>
-                            </div>
-                        </form>
-                    </div>
-                </div> */}
-
                 </div>
             }
         </div>
