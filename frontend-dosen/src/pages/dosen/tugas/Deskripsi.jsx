8521144fb32c94a6d814f3e39ea5702ebf86036d
Ridlo21 <inurrie99@gmail.com>
2024-01-20 00:31:08 +0700

    menu tugas

--- frontend-dosen/src/pages/dosen/tugas/Deskripsi.jsx
+++ frontend-dosen/src/pages/dosen/tugas/Deskripsi.jsx
@@ -8,6 +8,7 @@ import { Link, Navigate, useLocation } from "react-router-dom"
 import { Circles } from "react-loader-spinner"
 import axios from 'axios'
 import moment from "moment"
+import { FaReply } from 'react-icons/fa'
 
 const Deskripsi = () => {
     const dispatch = useDispatch()
@@ -17,9 +18,9 @@ const Deskripsi = () => {
     const [Mahasiswa, setMahasiswa] = useState([])
     const location = useLocation()
 
-    // useEffect(() => {
-    //     console.log(location.state)
-    // }, [location])
+    useEffect(() => {
+        console.log(location.state)
+    }, [location])
 
     useEffect(() => {
         const getDetailTugas = async () => {
@@ -130,55 +131,64 @@ const Deskripsi = () => {
                                     </Card>
                                     <Card className='shadow mt-3'>
                                         <Card.Body className='p-3'>
-                                            <div className='table-responsive'>
-                                                <Table>
-                                                    <thead>
-                                                        <tr className='border'>
-                                                            <th className='fw-bold py-3' style={{ backgroundColor: '#E9EAE1' }}>NO</th>
-                                                            <th className='fw-bold py-3' style={{ backgroundColor: '#E9EAE1' }}>NIM</th>
-                                                            <th className='fw-bold py-3' style={{ backgroundColor: '#E9EAE1' }}>Nama</th>
-                                                            <th className='fw-bold py-3' style={{ backgroundColor: '#E9EAE1' }}>Tanggal Pengumpulan</th>
-                                                            <th className='fw-bold py-3' style={{ backgroundColor: '#E9EAE1' }}>Prodi</th>
-                                                            <th className='fw-bold py-3' style={{ backgroundColor: '#E9EAE1' }}>Status</th>
-                                                            <th className='fw-bold py-3' style={{ backgroundColor: '#E9EAE1' }}>Aksi</th>
-                                                        </tr>
-                                                    </thead>
-                                                    <tbody>
-                                                        {Mahasiswa.length == '0' ?
-                                                            <tr className='border'>
-                                                                <td className='py-2 px-2 text-capitalize' align='center' colSpan={7}>
-                                                                    <Image src={dataBlank} className='mt-4 ' width={150} />
-                                                                    <p>Mahasiswa belum mengumpulkan tugas</p>
-                                                                </td>
-                                                            </tr>
-                                                            :
-
-                                                            Mahasiswa.map((item, index) => (
-                                                                <tr key={index} className='border'>
-                                                                    <td className='py-2 px-2 text-capitalize'>{index + 1}</td>
-                                                                    <td className='py-2 px-2 text-capitalize'>{item.nim}</td>
-                                                                    <td className='py-2 px-2 text-capitalize'>{item.mahasiswas[0].nama}</td>
-                                                                    <td className='py-2 px-2 text-capitalize'>{item.tanggal_pengumpulan == '' ? '-' : moment(item.tanggal_pengumpulan).format('DD MMMM YYYY')}</td>
-                                                                    <td className='py-2 px-2 text-capitalize'>{item.mahasiswas[0].prodis[0].nama_prodi}</td>
-                                                                    <td className='py-2 px-2 text-capitalize'>
-                                                                        {
-                                                                            item.status == 'terkumpul' ?
-                                                                                <span className="inline-block whitespace-nowrap rounded-[0.27rem] bg-[#17A2B8] px-[0.65em] pb-[0.25em] pt-[0.35em] text-center align-baseline text-[0.75em] font-bold leading-none text-white">Selesai</span>
-                                                                                :
-                                                                                <span className="inline-block whitespace-nowrap rounded-[0.27rem] bg-[#DC3545] px-[0.65em] pb-[0.25em] pt-[0.35em] text-center align-baseline text-[0.75em] font-bold leading-none text-white">Belum</span>
-                                                                        }
-                                                                    </td>
-                                                                    <td className='py-2 px-2 text-capitalize'>
-                                                                        <Link to="/detailTugas" state={{
-                                                                            kodeTgs: Tugas.code_tugas,
-                                                                            nim: item.nim
-                                                                        }} className={`btn btn-sm btn-info ${item.status == 'terkumpul' ? '' : 'disabled'}`}>Detail</Link>
-                                                                    </td>
+                                            <Row className='mb-3'>
+                                                <Col>
+                                                    <Link to='/tugas' className='bg-[#DC3545] py-1 px-2 rounded no-underline text-white inline-flex items-center gap-1'><FaReply /> Kembali</Link>
+                                                </Col>
+                                            </Row>
+                                            <Row>
+                                                <Col>
+                                                    <div className='table-responsive'>
+                                                        <Table>
+                                                            <thead>
+                                                                <tr className='border'>
+                                                                    <th className='fw-bold py-3' style={{ backgroundColor: '#E9EAE1' }}>NO</th>
+                                                                    <th className='fw-bold py-3' style={{ backgroundColor: '#E9EAE1' }}>NIM</th>
+                                                                    <th className='fw-bold py-3' style={{ backgroundColor: '#E9EAE1' }}>Nama</th>
+                                                                    <th className='fw-bold py-3' style={{ backgroundColor: '#E9EAE1' }}>Tanggal Pengumpulan</th>
+                                                                    <th className='fw-bold py-3' style={{ backgroundColor: '#E9EAE1' }}>Prodi</th>
+                                                                    <th className='fw-bold py-3' style={{ backgroundColor: '#E9EAE1' }}>Status</th>
+                                                                    <th className='fw-bold py-3' style={{ backgroundColor: '#E9EAE1' }}>Aksi</th>
                                                                 </tr>
-                                                            ))}
-                                                    </tbody>
-                                                </Table>
-                                            </div>
+                                                            </thead>
+                                                            <tbody>
+                                                                {Mahasiswa.length == '0' ?
+                                                                    <tr className='border'>
+                                                                        <td className='py-2 px-2 text-capitalize' align='center' colSpan={7}>
+                                                                            <Image src={dataBlank} className='mt-4 ' width={150} />
+                                                                            <p>Mahasiswa belum mengumpulkan tugas</p>
+                                                                        </td>
+                                                                    </tr>
+                                                                    :
+
+                                                                    Mahasiswa.map((item, index) => (
+                                                                        <tr key={index} className='border'>
+                                                                            <td className='py-2 px-2 text-capitalize'>{index + 1}</td>
+                                                                            <td className='py-2 px-2 text-capitalize'>{item.nim}</td>
+                                                                            <td className='py-2 px-2 text-capitalize'>{item.mahasiswas[0].nama}</td>
+                                                                            <td className='py-2 px-2 text-capitalize'>{item.tanggal_pengumpulan == '' ? '-' : moment(item.tanggal_pengumpulan).format('DD MMMM YYYY')}</td>
+                                                                            <td className='py-2 px-2 text-capitalize'>{item.mahasiswas[0].prodis[0].nama_prodi}</td>
+                                                                            <td className='py-2 px-2 text-capitalize'>
+                                                                                {
+                                                                                    item.status == 'terkumpul' ?
+                                                                                        <span className="inline-block whitespace-nowrap rounded-[0.27rem] bg-[#17A2B8] px-[0.65em] pb-[0.25em] pt-[0.35em] text-center align-baseline text-[0.75em] font-bold leading-none text-white">Selesai</span>
+                                                                                        :
+                                                                                        <span className="inline-block whitespace-nowrap rounded-[0.27rem] bg-[#DC3545] px-[0.65em] pb-[0.25em] pt-[0.35em] text-center align-baseline text-[0.75em] font-bold leading-none text-white">Belum</span>
+                                                                                }
+                                                                            </td>
+                                                                            <td className='py-2 px-2 text-capitalize'>
+                                                                                <Link to="/detailTugas" state={{
+                                                                                    kodeTgs: Tugas.code_tugas,
+                                                                                    nim: item.nim
+                                                                                }} className={`btn btn-sm btn-info ${item.status == 'terkumpul' ? '' : 'disabled'}`}>Detail</Link>
+                                                                            </td>
+                                                                        </tr>
+                                                                    ))}
+                                                            </tbody>
+                                                        </Table>
+                                                    </div>
+                                                </Col>
+                                            </Row>
                                         </Card.Body>
                                     </Card>
                                 </Col>
