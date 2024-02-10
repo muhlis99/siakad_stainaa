852d251ab1fbb99cd3fb1e1c28308015c393482a
ole <muklek99@gmail.com>
2024-02-10 23:38:16 +0700

    jurnal dosen

--- backend-siamdos/router/jurnalDosenRoute.js
+++ backend-siamdos/router/jurnalDosenRoute.js
@@ -11,5 +11,6 @@ route.put('/update/:id', jurnalDosenController.put)
 route.put('/delete/:id', jurnalDosenController.delete)
 route.get('/getMakulByDosen/:nipy/:thn/:smt/:jnj/:fks/:prd', jurnalDosenController.getMakulByDosen)
 route.get('/getPertemuanByDosen/:codeJadkul', jurnalDosenController.getPertemuanByDosen)
+route.get('/getStatusJurnal/:codeJadper', jurnalDosenController.getStatusJurnal)
 
 module.exports = route
